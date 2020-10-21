6ed1bc9a6b7be464c1ff6e1b7aa8ce0c3a9cf55d
Tracks12 <cardinal.florian.erwan@gmail.com>
2020-10-21 10:03:27 +0200

    MàJ template components

--- scripts/main.js
+++ scripts/main.js
@@ -42,15 +42,52 @@ const appView = {
 				<li v-for="user in users">{{ user }}</li>
 			</ul>
 			<form>
-				<input type="text" v-model="newUser" />
-				<input type="submit" v-on:click.prevent="addUser" />
+				<input placeholder="username" type="text" v-model="newUser" />
+				<input type="submit" @click.prevent="addUser" />
+			</form>
+
+			<table>
+				<tr>
+					<th>Nr.</th>
+					<th>Name</th>
+					<th>Price</th>
+				</tr>
+				<tr v-for="item in products">
+					<td>{{ item.id }}</td>
+					<td>{{ item.name }}</td>
+					<td>{{ item.price }} €</td>
+				</tr>
+			</table>
+
+			<form>
+				<input placeholder="product" type="text" v-model="product" />
+				<input placeholder="price" type="number" v-model="price" />
+				<input type="submit" @click.prevent="addProduct" />
 			</form>
 		</div>
 	`,
 
   methods: {
     addUser: function () {
-      if (this.newUser) this.users.push(this.newUser);
+      let newUser = this.newUser;
+      if (newUser) {
+        this.users.push(newUser);
+        this.newUser = null;
+      }
+    },
+
+    addProduct: function () {
+      let product = {
+        id: this.products.length + 1,
+        name: this.product,
+        price: this.price,
+      };
+
+      if (product.name && product.price) {
+        this.products.push(product);
+        this.product = null;
+        this.price = null;
+      }
     },
   },
 
@@ -58,6 +95,13 @@ const appView = {
     return {
       users: ["clément", "jean-marc"],
       newUser: "",
+      products: [
+        { id: 1, name: "Zalman Z11", price: 52.0 },
+        { id: 2, name: "AMD Ryzen 7", price: 347.0 },
+        { id: 3, name: "GigaBytes Quantum Z20", price: 77.0 },
+      ],
+      product: "",
+      price: "",
     };
   },
 };
