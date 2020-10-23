72b163b4fd3c089aa3906840805f03bc95cf0850
Tracks12 <cardinal.florian.erwan@gmail.com>
2020-10-23 20:54:10 +0200

    product save in json file

--- api/route.php
+++ api/route.php
@@ -1,16 +1,40 @@
 <?php
 	switch(service::isInput($_SERVER['REQUEST_URI'])) {
 		case "/api/?getUsers":
-			http_response_code(200);
-			echo(file_get_contents("data/users.json"));
-			break;
+			$return = [
+				"code"     => http_response_code(200),
+				"response" => json_decode(file_get_contents("data/users.json"))
+			]; break;
 
 		case "/api/?getProducts":
-			http_response_code(200);
-			echo(file_get_contents("data/products.json"));
-			break;
+			$return = [
+				"code"     => http_response_code(200),
+				"response" => json_decode(file_get_contents("data/products.json"))
+			]; break;
+
+		case "/api/?insertProduct":
+			$post = service::checkArray($_POST);
+			$json = json_decode(file_get_contents("data/products.json"));
+
+			$post = [
+				"id"    => intval($post["id"]),
+				"name"  => $post["name"],
+				"price" => floatval($post["price"])
+			];
+
+			array_push($json, $post);
+
+			$return = [
+				"code"     => http_response_code(200),
+				"response" => file_put_contents("data/products.json", json_encode($json)) ? true : false
+			]; break;
 
 		default:
-			die();
-			break;
+			$return = [
+				"code"     => http_response_code(404),
+				"response" => false
+			]; break;
 	}
+
+	echo(json_encode($return));
+	die();
